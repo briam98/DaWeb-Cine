7cf546779f14208ee2c30bf941bf5dd02a52b968
JosePina98 <jose_pina_98@hotmail.com>
2020-05-07 18:40:47 +0200

    Avanzamos con express

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,14 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+	var datos = {
+		cines: [
+			{nombre: "cine1", ubicacion: "ubi1", aforo: "1", numSalas: "1", imagen: "images/cine1.png"},
+			{nombre: "cine2", ubicacion: "ubi2", aforo: "2", numSalas: "2", imagen: "images/cine1.png"},
+			{nombre: "cine3", ubicacion: "ubi3", aforo: "3", numSalas: "3", imagen: "images/cine1.png"}
+		]
+	}
+	res.render('index', datos);
 });
 
 module.exports = router;
