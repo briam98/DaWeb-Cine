a355c909d48d93c4efc009d51c62752342016cdc
JosePina98 <jose_pina_98@hotmail.com>
2020-05-07 19:04:02 +0200

    Añadimos el carrusel de fotos

--- routes/index.js
+++ routes/index.js
@@ -5,9 +5,9 @@ var router = express.Router();
 router.get('/', function(req, res, next) {
 	var datos = {
 		cines: [
-			{nombre: "cine1", ubicacion: "ubi1", aforo: "1", numSalas: "1", imagen: "images/cine1.png"},
-			{nombre: "cine2", ubicacion: "ubi2", aforo: "2", numSalas: "2", imagen: "images/cine1.png"},
-			{nombre: "cine3", ubicacion: "ubi3", aforo: "3", numSalas: "3", imagen: "images/cine1.png"}
+			{nombre: "cine1", ubicacion: "ubi1", aforo: "1", numSalas: "1", imagen1: "images/cine1.png", imagen2: "images/cine2.png", imagen3: "images/cine3.png"},
+			{nombre: "cine2", ubicacion: "ubi2", aforo: "2", numSalas: "2", imagen1: "images/cine1.png", imagen2: "images/cine2.png", imagen3: "images/cine3.png"},
+			{nombre: "cine3", ubicacion: "ubi3", aforo: "3", numSalas: "3", imagen1: "images/cine1.png", imagen2: "images/cine2.png", imagen3: "images/cine3.png"}
 		]
 	}
 	res.render('index', datos);
